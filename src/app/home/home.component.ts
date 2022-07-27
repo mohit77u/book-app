dba3ebd37bf4940a05bcb6a19cc55e665921ccfe
mohit77u <mohiit77u@gmail.com>
2022-07-27 23:28:12 +0530

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   // step = 2 ; // hidden by default
@@ -12,6 +12,26 @@ export class HomeComponent implements OnInit {
   changeStep(event: any, num: any) { 
     this.step = num
  }
+
+//  define variables
+  name: string = '';
+  email: string = '';
+  mobile: string = '';
+  book_orientation: string = '';
+  paper_size: string = '';
+  paper_count: string = '';
+  book_content: string = '';
+  sample: string = '';
+  quality: string = '';
+  gsm: string = '';
+  interior_color: string = '';
+  cover_page: string = '';
+  cover_quality: string = '';
+  binding_type: string = '';
+  book_quantity: string = '';
+  book_name: string = '';
+  // cover_quality: string = '';
+  // cover_quality: string = '';
   constructor() { 
     
   }
